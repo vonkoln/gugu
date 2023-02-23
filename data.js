c5c42f1537f8901aa70c0f8fc9b0c69dc985973f
brunovonkoln <brunovonkoln@gmail.com>
2023-02-23 11:44:37 -0300

    ..

---
+++ data.js
@@ -0,0 +1,62 @@
+export default  [ 
+  {
+    title:"Inno ADORO TE DEVOTE",
+    artist: "Tommaso D'Aquino",
+    cover: "tom.jfif",
+    file: "tom.mp3",
+  },  
+  {
+    title:"Air on the G String (Suite No. 3, BWV 1068)",
+    artist: "J. S. Bach",
+    cover: "air.jfif",
+    file: "air.mp3",
+  },  
+  {
+    title:"Fratres",
+    artist: "Arvo Pärt",
+    cover: "arvo.jfif",
+    file: "fra.mp3",
+  },  
+  {
+    title:"Spiegel im Spiegel",
+    artist: "Arvo Pärt",
+    cover: "spi.jpg",
+    file: "spi.mp3",
+  },  
+  {
+    title:"Coro",
+    artist: "MONJES DEL MONASTERIO DE SANTO DOMINGO DE SILOS",
+    cover: "coro.jpg",
+    file: "coro.mp3",
+  },  
+  {
+    title:"Laudate Dominum",
+    artist: "Notre Dame Paris",
+    cover: "lau.jpg",
+    file: "lau.mp3",
+  },  
+  {
+    title:"Mare Nostrum",
+    artist: "Lior Elmaleh e Irini Derebei",
+    cover: "mare.jpg",
+    file: "mare.mp3",
+  },  
+  {
+    title:"Miserere I Allegri I Ars Nova Copenhagen",
+    artist: "Paul Hillier",
+    cover: "mise.jfif",
+    file: "mise.mp3",
+  },   
+  {
+    title:"Miserere mei, Deus ",
+    artist: "Gregorio Allegri",
+    cover: "greg.jfif",
+    file: "greg.mp3",
+  },   
+  {
+    title:"Gregoria Latín",
+    artist: "Kyrie Eleison",
+    cover: "lat.jfif",
+    file: "lat.mp3",
+  } 
+]
